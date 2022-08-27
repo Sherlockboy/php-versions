fabfdd5f81cc06643e0f10da12da3914cee80546
Khurshidbek Kobilov <kabilovhurshid5@gmail.com>
2022-08-27 21:52:53 +0500

    php 8.1

--- BlogData.php
+++ BlogData.php
@@ -3,25 +3,10 @@
 class BlogData
 {
     public function __construct(
-        private string $title,
-        private bool $state,
-        private ?DateTime $publishedAt = null
+        public readonly string $title,
+        public readonly bool $state,
+        public readonly ?DateTime $publishedAt = null
     )
     {
-    }
-
-    public function getTitle(): string
-    {
-        return $this->title;
-    }
-
-    public function getState(): bool
-    {
-        return $this->state;
-    }
-
-    public function getPublishedAt(): ?DateTime
-    {
-        return $this->publishedAt;
     }
 }
