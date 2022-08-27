2be9a4bad379bc881e4435a34f802bb0f04a9e59
Khurshidbek Kobilov <kabilovhurshid5@gmail.com>
2022-08-27 21:45:37 +0500

    php 5.6

--- BlogData.php
+++ BlogData.php
@@ -2,4 +2,52 @@
 
 class BlogData
 {
+    /** @var string */
+    private $title;
+
+    /** @var bool */
+    private $state;
+
+    /** @var DateTime|null */
+    private $publishedAt;
+
+    /**
+     * @param string $title
+     * @param bool $state
+     * @param DateTime|null $publishedAt
+     */
+    public function __construct(
+        $title,
+        $state,
+        $publishedAt
+    )
+    {
+        $this->title = $title;
+        $this->state = $state;
+        $this->publishedAt = $publishedAt;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @return bool
+     */
+    public function getState()
+    {
+        return $this->state;
+    }
+
+    /**
+     * @return DateTime|null
+     */
+    public function getPublishedAt()
+    {
+        return $this->publishedAt;
+    }
 }
